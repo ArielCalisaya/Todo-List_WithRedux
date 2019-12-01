8a46dae9860427408c790132fd5ad6b17c8385c8
Ariel Calisaya <calisayaariel62@gmail.com>
2019-12-01 19:53:27 -0300

    Todo no todo for now

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
     return(
         <Provider store={store}>
             <main className="lg:bg-gray-800 bg-gray-900 items-center flex-col flex  text-center justify-center text-xl" >
-                <h1 className="text-4xl text-white pt-8 pb-6 ">Todo App Main App z-initial</h1>
+                <h1 className="text-4xl text-white pt-8 pb-6 ">Todo App</h1>
                 <InputFilter />
                 <List/>
             </main>
--- src/components/todoInput&Filter.jsx
+++ src/components/todoInput&Filter.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import Filter from './filter';
 import { connect } from 'react-redux';
 
-class InputFilter extends React.Component {
-
-    render(){
+const InputFilter = ({AddTodo}) => {
         return(
-
             <div className="lg:pl-64 lg:pr-64 flex lg:w-full  flex-col  lg:justify-between">
             <div className="">
-                <button type="button" className="btn  mb-6 btn-primary rounded-full" data-toggle="modal" data-target="#exampleModalCenter">
+                <button type="button" className="btn  mb-6 bg-blue-600 text-gray-200 rounded-full" data-toggle="modal" data-target="#Modal-Input">
                   +
                 </button>
             </div>
@@ -17,21 +14,25 @@ class InputFilter extends React.Component {
                 <Filter/>
             </div>
 
-                <div className="modal fade" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div className="modal fade" id="Modal-Input" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                   <div className="modal-dialog modal-dialog-centered" role="document">
                     <div className="modal-content">
-                      <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
+                      <div className="modal-header block">
+                      <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                      </button>
+                        <h5 className="modal-title text-center" id="exampleModalCenterTitle">Añadir nuevo To do</h5>
                       </div>
-                      <div className="modal-body">
-                        ...
+                      <div className="modal-body flex flex-col">
+                        <input className="mb-6 p-2 border-gray-700 rounded border-t border-b border-r border-l" type="text" placeholder="To do"/>
+                        <textarea className="w-full p-2 h-48 border-gray-700 rounded border-t border-b border-r border-l" placeholder="Description"></textarea>
                       </div>
                       <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
+                        <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
+                        <button
+                        onClick={() => AddTodo()}
+                        type="button"
+                        className="btn btn-success">Add to do</button>
                       </div>
                     </div>
                   </div>
@@ -39,12 +40,17 @@ class InputFilter extends React.Component {
             </div>
 
         )
-    }
-
 }
 const mapStateToProps = state => ({
     LisTodo: state.todo,
 })
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+    AddTodo(TodoId) {
+        dispatch({
+            type:"AGREGAR_TODO",
+            TodoId
+        })
+    }
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(InputFilter)
