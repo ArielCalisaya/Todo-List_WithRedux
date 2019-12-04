a19cb332c51919814499da3202c6d15f813fa462
Ariel Calisaya <calisayaariel62@gmail.com>
2019-12-04 12:58:01 -0300

    axios and redux-thunk for api

--- src/actions/index.spec.js
+++ src/actions/index.spec.js
@@ -2,14 +2,17 @@ import * as actions from './index';
 
 describe('todo actions', () => {
     it('addTodo should create ADD_TODO action' => {
-        expect(actions.addTodo('Output String')).toEqual({
+        expect(actions.addTodo('Output String'))
+        .toEqual({
             type:'ADD_TODO',
             id: 0,
             text: 'Output String'
         })
     })
+
     it('toggleTodo should create TOGGLE_TODO action' => {
-        expect(actions.toggleTodo(1)).toEqual({
+        expect(actions.toggleTodo(1))
+        .toEqual({
             type:'TOGGLE_TODO',
             id: 1
         })
